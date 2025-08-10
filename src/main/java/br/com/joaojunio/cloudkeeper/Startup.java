77b538119c65205a57a0b7f8a3fcbb98bcc4a889
joao_junio <joaojunio818@gmail.com>
2025-08-10 16:52:50 -0300

    Iniciando backend

---
+++ src/main/java/br/com/joaojunio/cloudkeeper/Startup.java
@@ -0,0 +1,13 @@
+package br.com.joaojunio.cloudkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaojunio/cloudkeeper/CloudkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaojunio.cloudkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
