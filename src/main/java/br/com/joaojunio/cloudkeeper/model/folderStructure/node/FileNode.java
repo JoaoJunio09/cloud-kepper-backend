5d973dd0f3fce3084c97b00dddef50de680a8ba1
joao_junio <joaojunio818@gmail.com>
2025-10-12 16:44:13 -0300

    Adicionando data de modificação aos arquivos e pastas

--- src/main/java/br/com/joaojunio/cloudkeeper/model/folderStructure/node/FileNode.java
+++ src/main/java/br/com/joaojunio/cloudkeeper/model/folderStructure/node/FileNode.java
@@ -1,18 +1,27 @@
 package br.com.joaojunio.cloudkeeper.model.folderStructure.node;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class FileNode extends Node {
 
     public String fileId;
     public String fileType;
     public Long size;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    public Date datetime;
 
     public FileNode() {
         setType("file");
+        this.datetime = new Date();
     }
 
     public FileNode(String fileId, String name, String fileType, Long size) {
         setType("file");
         setName(name);
+        this.datetime = new Date();
         this.fileId = fileId;
         this.fileType = fileType;
         this.size = size;
@@ -40,5 +49,14 @@ public class FileNode extends Node {
 
     public void setSize(Long size) {
         this.size = size;
+    }
+
+    public String getDatetime() {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return format.format(datetime);
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
     }
 }
--- src/main/java/br/com/joaojunio/cloudkeeper/model/folderStructure/node/FolderNode.java
+++ src/main/java/br/com/joaojunio/cloudkeeper/model/folderStructure/node/FolderNode.java
@@ -1,9 +1,12 @@
 package br.com.joaojunio.cloudkeeper.model.folderStructure.node;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -11,14 +14,18 @@ public class FolderNode extends Node {
     public List<Node> children = new ArrayList<>();
     @JsonSerialize(using = ToStringSerializer.class)
     private String id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    private Date datetime;
 
     public FolderNode() {
         setType("folder");
+        this.datetime = new Date();
     }
 
     public FolderNode(String name) {
         setType("folder");
         setName(name);
+        this.datetime = new Date();
 
         Random generate = new Random();
         this.id = String.valueOf(generate.nextLong());
@@ -42,5 +49,14 @@ public class FolderNode extends Node {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getDatetime() {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return format.format(datetime);
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
     }
 }
